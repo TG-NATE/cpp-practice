a03a9264e156eb0b7d0f9329064a0558b52195e1
Nathan Ireland <nathangireland@gmail.com>
2026-05-06 22:13:05 -0500

    Started isCardValid with implmentaion of luhn's algorithm

---
+++ Projects/isCardValid.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+std::string testCards[] = {
+
+    // ---------------- VALID CARD NUMBERS ----------------
+
+    // Visa
+    "4111111111111111",
+    "4012888888881881",
+
+    // MasterCard
+    "5555555555554444",
+    "5105105105105100",
+
+    // American Express
+    "378282246310005",
+    "371449635398431",
+
+    // Discover
+    "6011111111111117",
+    "6011000990139424",
+
+    // JCB
+    "3530111333300000",
+    "3566002020360505",
+
+    // Diners Club
+    "30569309025904",
+    "38520000023237",
+
+    // ---------------- INVALID CARD NUMBERS ----------------
+
+    // One digit changed from valid numbers
+    "4111111111111112",
+    "5555555555554445",
+    "378282246310006",
+    "6011111111111118",
+
+    // Random invalid lengths
+    "1234567890123456",
+    "9999999999999999",
+    "1111111111111111",
+
+    // Too short
+    "411111111111",
+
+    // Too long
+    "41111111111111111111",
+
+    // Edge-ish cases
+    "0000000000000000",
+    "2222222222222222"
+};
+
+bool isCardValid(std::string cardNumber);
+
+int main(){
+
+    int testCardArraySize = sizeof(testCards)/sizeof(testCards[0]);
+
+    for(std::string card:testCards){
+
+        if (card.size() > 12 && card.size() < 20)
+        {
+            std::cout << "The card number is the correct length" <<  "\n";
+            isCardValid(card);
+
+        }else{
+            std::cout << "The card number is the incorrect length" <<  "\n";
+        }
+        
+    }
+}
+
+
+bool isCardValid(std::string cardNumber){
+    for(int i = 0; i < cardNumber.size(); i++){
+        std::cout << cardNumber.at(i) <<  "\n";
+    }
+}
