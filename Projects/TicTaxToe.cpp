44cfc51ef7d60077f84969a66dce88fcda5725a9
Nathan Ireland <nathangireland@gmail.com>
2026-05-12 13:52:33 -0500

    Completed Tic-Tax-Toe

--- Projects/TicTaxToe.cpp
+++ Projects/TicTaxToe.cpp
@@ -4,45 +4,65 @@
 
 
 void drawBoard(char *spaces);
+void clearBoard(char *spaces);
 void playerMove(char *spaces, char player);
 void computaMove(char *spaces, char computa);
-bool checkWinner(char *spaces, char player, char computa);
-bool checkTie(char *spaces);
+bool checkWinner(char *spaces, char player, char computa, int &playerWins, int &computaWins);
+bool checkTie(char *spaces, int &ties);
 
 
 int main(){
     char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
     char player = 'X';
     char computa = 'O';
+    int playerWins = 0;
+    int computaWins = 0;
+    int ties = 0;
     bool running = true;
-
-    drawBoard(spaces);
+    bool anotherRound = true;
+    std::string anotherRoundChoice = "T";
 
     do
     {
-        playerMove(spaces, player);
         drawBoard(spaces);
 
-        running = checkWinner(spaces, player, computa);
-
-        if (running == false)
+        do
         {
-            break;
-        }
+            playerMove(spaces, player);
+            drawBoard(spaces);
+
+            if (!checkWinner(spaces, player, computa, playerWins, computaWins)|| !checkTie(spaces, ties))
+            {
+                clearBoard(spaces);
+                running = false;
+                break;
+            }
+            
+
+            computaMove(spaces, computa);
+            drawBoard(spaces);
+
+        
+            if (!checkWinner(spaces, player, computa, playerWins, computaWins)|| !checkTie(spaces, ties))
+            {
+                clearBoard(spaces);
+                running = false;
+                break;
+            }
+
         
 
-        computaMove(spaces, computa);
-        drawBoard(spaces);
 
-        running = checkWinner(spaces, player, computa);
-        
-        
+        } while (running == true);
 
-    } while (running == true);
-    
+        std::cout << "Player Wins: "<< playerWins << " Computa Wins: "<< computaWins << " Ties: "<< ties << "\n";
 
-    
+        std::cout << "Do you want to play again? (Y)es (N)o \n";
+        std::cin >> anotherRoundChoice;
+        toupper(anotherRoundChoice.at(0)) == 'Y' ? anotherRound = true, running = true : anotherRound = false;
 
+
+    }while (anotherRound == true);
     
     return 0;
 }
@@ -55,6 +75,12 @@ void drawBoard(char *spaces){
     std::cout << "-------|-------|-------\n";
     std::cout << "   " << spaces[6] << "   |   " << spaces[7] << "   |   " << spaces[8] << "   \n";
     std::cout << "***********************\n";
+}
+
+void clearBoard(char *spaces){
+    for(int i = 0; i < 9; i++){
+        spaces[i] = ' ';
+    }
 }
     
 void playerMove(char *spaces, char player){
@@ -108,56 +134,60 @@ void computaMove(char *spaces, char computa){
 
 }
 
-bool checkWinner(char *spaces, char player, char computa){
-    char winner;
+bool checkWinner(char *spaces, char player, char computa, int &playerWins, int &computaWins){
+    char winner = ' ';
 
-    if(spaces[0] == player && spaces[1] == player && spaces[2] == player || spaces[0] == computa && spaces[1] == computa && spaces[2] == computa)
+    if(spaces[0] != ' ' && spaces[0] == spaces[1] && spaces[1] == spaces[2])
     {
         winner = spaces[0];
-    }else if (spaces[0] == player && spaces[4] == player && spaces[8] == player || spaces[0] == computa && spaces[4] == computa && spaces[8] == computa)
+    }else if (spaces[0] != ' ' && spaces[0] == spaces[4] && spaces[4] == spaces[8])
     {
         winner = spaces[0];
-    }else if (spaces[0] == player && spaces[3] == player && spaces[6] == player || spaces[0] == computa && spaces[3] == computa && spaces[6] == computa)
+    }else if (spaces[0] != ' ' && spaces[0] == spaces[3] && spaces[3] == spaces[6])
     {
         winner = spaces[0];
-    }else if (spaces[3] == player && spaces[4] == player && spaces[5] == player || spaces[3] == computa && spaces[4] == computa && spaces[5] == computa)
+    }else if (spaces[3] != ' ' && spaces[3] == spaces[4] && spaces[4] == spaces[5])
     {
         winner = spaces[4];
-    }else if (spaces[6] == player && spaces[7] == player && spaces[8] == player || spaces[6] == computa && spaces[7] == computa && spaces[8] == computa)
+    }else if (spaces[6] != ' ' && spaces[6] == spaces[7] && spaces[7] == spaces[8])
     {
         winner = spaces[7];
-    }else if (spaces[1] == player && spaces[4] == player && spaces[7] == player || spaces[1] == computa && spaces[4] == computa && spaces[7] == computa)
+    }else if (spaces[1] != ' ' && spaces[1] == spaces[4] && spaces[4] == spaces[7])
     {
         winner = spaces[1];
-    }else if (spaces[2] == player && spaces[5] == player && spaces[8] == player || spaces[2] == computa && spaces[5] == computa && spaces[8] == computa)
+    }else if (spaces[2] != ' ' && spaces[2] == spaces[5] && spaces[5] == spaces[8])
+    {
+        winner = spaces[2];
+    }else if (spaces[2] != ' ' && spaces[2] == spaces[4] && spaces[4] == spaces[6])
     {
         winner = spaces[2];
     }
     
     if(winner == player){
         std::cout << "You Won!\n";
-        return 0;
+        playerWins++;
+        return false;
     }else if(winner == computa){
         std::cout << "Computa Won!\n";
-        return 0;
+        computaWins++;
+        return false;
     }else{
-        return 1;
+        return true;
     }
 }
 
 
-bool checkTie(char *spaces){
+bool checkTie(char *spaces, int &ties){
 
-    for(int i = 0; i < 10; i++){
-
+    for(int i = 0; i < 9; i++){
         if (spaces[i] == ' ')
         {
-            return 1;
+            std::cout << "empty space found at: " << i << "\n";
+            return true;
         }
-        
-
     }
 
-    std::cout << "There's a Tie\n"; 
-    return 0;
+    std::cout << "There's a Tie \n";
+    ties++;
+    return false;
 }
